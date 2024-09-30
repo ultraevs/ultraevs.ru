134c2329002076042ffab6923d58fad4998192eb
Mikhail <98035893+ultraevs@users.noreply.github.com>
2024-09-30 19:06:21 +0300

    Update MainRouter.go

--- go-backend/internal/api/router/MainRouter.go
+++ go-backend/internal/api/router/MainRouter.go
@@ -9,5 +9,5 @@ func (router *Router) MainRoutes() {
 	router.engine.GET("/gagarin", controller.GetSmolathon)
 	router.engine.GET("/urbaton", controller.GetUrbaton)
 	router.engine.GET("/moretech5", controller.GetMoretech5)
-	router.engine.GET("/inno"), controoler.GetInnoHack)
+	router.engine.GET("/inno", controoler.GetInnoHack)
 }
